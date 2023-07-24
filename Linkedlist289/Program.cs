77845ca6372c81e38b24415c5cbc6cb87cd92d1c
Sachinmahajana <sachinmahajan782@gmail.com>
2023-07-24 17:43:30 +0530

    [Sachin] Adding Welcome Message

---
+++ Linkedlist289/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Linkedlist289
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Linkedlist program");
+        }
+    }
+}
