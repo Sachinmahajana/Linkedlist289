6ffbc34c8ee5a8a1aad91e6c2d47b2885a99b684
Sachinmahajana <sachinmahajan782@gmail.com>
2023-08-04 00:13:08 +0530

    [Sachin] Adding UC1-CreateLinkedlist

--- Linkedlist289/Program.cs
+++ Linkedlist289/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Linkedlist289
+﻿using System.Collections.Generic;
+
+namespace Linkedlist289
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Linkedlist program");
+            Linkedlist linkedList = new Linkedlist();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1 : Add Data");
+                int Option = Convert.ToInt32(Console.ReadLine());
+                switch (Option)
+                {
+                    case 1:
+                        linkedList.Add(30);
+                        linkedList.Add(56);
+                        linkedList.Add(70);
+                        break;
+                }
+            }
         }
     }
 }
